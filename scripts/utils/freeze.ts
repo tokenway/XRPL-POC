c58a77021e4e0bfbb585652e66ae912dffc68c2a
Couple-shine <BlackHole1225.dev@gmail.com>
2025-07-05 13:11:48 +0300

    feat: create freeze utility function

---
+++ scripts/utils/freeze.ts
@@ -0,0 +1,48 @@
+import { xrplClient } from "../setup/client"
+import { XRPL_FLAGS, XRPL_TX_TYPES } from "../constants"
+import { AccountSet, TrustSet, Wallet } from "xrpl"
+import { metaResultOK } from "./helpers"
+
+export async function freezeGlobally(issuerAddress: string, issuerWallet: Wallet) {
+  await xrplClient.connect()
+
+  const tx: AccountSet = {
+    TransactionType: XRPL_TX_TYPES.ACCOUNT_SET,
+    Account: issuerAddress,
+    SetFlag: XRPL_FLAGS.GLOBAL_FREEZE,
+  }
+
+  const res = await xrplClient.submitAndWait(tx, { wallet: issuerWallet })
+
+  if (!metaResultOK(res.result.meta)) throw new Error("Global freeze failed")
+  console.log("Global freeze enabled.")
+
+  await xrplClient.disconnect()
+}
+
+export async function freezeTrustLine(
+  issuerAddress: string,
+  holderAddress: string,
+  currencyCode: string,
+  issuerWallet: Wallet
+) {
+  await xrplClient.connect()
+
+  const tx: TrustSet = {
+    TransactionType: XRPL_TX_TYPES.TRUST_SET,
+    Account: issuerAddress,
+    LimitAmount: {
+      currency: currencyCode,
+      issuer: holderAddress,
+      value: "0",
+    },
+    Flags: XRPL_FLAGS.TF_SET_FREEZE,
+  }
+
+  const res = await xrplClient.submitAndWait(tx, { wallet: issuerWallet })
+
+  if (!metaResultOK(res.result.meta)) throw new Error("Individual trust line freeze failed")
+  console.log(`Trust line frozen: ${holderAddress}`)
+
+  await xrplClient.disconnect()
+}
